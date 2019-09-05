c40e208892c700c71a6fed9d01a72f1d49dc696d
Muhammad Bukhori Muslim <mosleim@gmail.com>
2019-09-05 15:31:06 +0700

    initial commits

--- main.go
+++ main.go
@@ -4,17 +4,17 @@ import "github.com/jinzhu/gorm"
 
 // Syakhsun :
 type Syakhsun struct {
-	ID   uint16
-	Name string
-	Aba  uint16
-	Ummi uint16
+	*gorm.Model
+	Name string `gorm:"name" json:"name"`
+	Aba  uint16 `gorm:"aba" json:"aba"`
+	Ummi uint16 `gorm:"ummi" json:"ummi"`
 }
 
 // Zawaj :
 type Zawaj struct {
 	*gorm.Model
-	Zauj   uint16
-	Zaujah uint16
+	Zauj   uint16 `gorm:"zauj" json:"zauj"`
+	Zaujah uint16 `gorm:"zaujah" json:"zaujah"`
 }
 
 func main() {
